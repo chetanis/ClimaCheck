c4ac02f401b0d5a7fb2dde8f7adc54005f0d1807
chetanis <anis.chetouane@etu.usthb.dz>
2023-11-11 21:35:34 +0100

    Adding WeeklyForcast ScrollView

--- lib/widgets/Temperature_widget.dart
+++ lib/widgets/Temperature_widget.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class TemperatureWidget extends StatefulWidget {
-  final int degre, hour;
+  final int degre;
+  final String hour;
+  final bool current = false;
 
-  const TemperatureWidget({super.key, required this.degre, required this.hour});
+  const TemperatureWidget({super.key, required this.degre, required this.hour, current});
 
   @override
   State<TemperatureWidget> createState() => _TemperatureWidgetState();
@@ -12,14 +14,23 @@ class TemperatureWidget extends StatefulWidget {
 class _TemperatureWidgetState extends State<TemperatureWidget> {
   @override
   Widget build(BuildContext context) {
+    Color color =const Color.fromARGB(107, 46, 51, 90);
+    if (widget.current) {
+       color =const Color.fromARGB(255, 90, 46, 46);
+    }
     return Container(
-      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+      padding: const EdgeInsets.fromLTRB(4, 10, 4, 10),
       decoration: BoxDecoration(
-          border: Border.all(width: 2),
+          color: color,
+          border: Border.all(
+              width: 1, color: const Color.fromARGB(162, 151, 112, 164)),
           borderRadius: BorderRadius.circular(50)),
       child: Column(
         children: [
-          Text('${widget.hour}PM',style: const TextStyle(color: Colors.white),),
+          Text(
+            widget.hour,
+            style: const TextStyle(color: Colors.white),
+          ),
           const SizedBox(height: 10),
           const Image(
             image: AssetImage('images/Moon_cloud_mid_rain.png'),
@@ -27,7 +38,10 @@ class _TemperatureWidgetState extends State<TemperatureWidget> {
             height: 50,
           ),
           const SizedBox(height: 10),
-          Text('${widget.degre}°',style: const TextStyle(color: Colors.white),),
+          Text(
+            '${widget.degre}°',
+            style: const TextStyle(color: Colors.white),
+          ),
         ],
       ),
     );
--- lib/widgets/panel_widget.dart
+++ lib/widgets/panel_widget.dart
@@ -1,35 +1,156 @@
+import 'dart:math';
+
 import 'package:clima_check/widgets/Temperature_widget.dart';
 import 'package:flutter/material.dart';
 
-class PanelWidget extends StatelessWidget {
-  final ScrollController controller ;
+enum ForecastType { hourly, weekly }
+
+class PanelWidget extends StatefulWidget {
+  final ScrollController controller;
 
   const PanelWidget({
     Key? key,
     required this.controller,
-  }) :super(key: key);
+  }) : super(key: key);
+
+  @override
+  State<PanelWidget> createState() => _PanelWidgetState();
+}
+
+class _PanelWidgetState extends State<PanelWidget> {
+  ForecastType currentForecast = ForecastType.hourly;
 
   @override
   Widget build(BuildContext context) {
     return ListView(
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
       children: [
-        DegreeWidget(),
-
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  currentForecast = ForecastType.hourly;
+                });
+              },
+              child: const Text(
+                'Hourly Forecast',
+                style: TextStyle(color: Color.fromARGB(121, 255, 255, 255)),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  currentForecast = ForecastType.weekly;
+                });
+              },
+              child: const Text(
+                'Weekly Forecast',
+                style: TextStyle(color: Color.fromARGB(121, 255, 255, 255)),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: currentForecast == ForecastType.hourly
+                        ? const [
+                          Color.fromARGB(70, 198, 140, 236),
+                          Color.fromARGB(200, 198, 140, 236),
+                          Color.fromARGB(70, 198, 140, 236)
+                        ]
+                        : const [Color.fromARGB(27, 255, 255, 255),Color.fromARGB(27, 255, 255, 255)],
+                )),
+                height: 2,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: currentForecast == ForecastType.weekly
+                      ? const [
+                          Color.fromARGB(70, 198, 140, 236),
+                          Color.fromARGB(200, 198, 140, 236),
+                          Color.fromARGB(70, 198, 140, 236)
+                        ]
+                      : const [Color.fromARGB(27, 255, 255, 255),Color.fromARGB(27, 255, 255, 255)],
+                )),
+                height: 2,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        buildForecast(),
       ],
     );
   }
-  
-  Widget DegreeWidget() => const Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      TemperatureWidget(degre: 10, hour: 1),
-      TemperatureWidget(degre: 15, hour: 2),
-      TemperatureWidget(degre: 17, hour: 3),
-      TemperatureWidget(degre: 14, hour: 4),
-      TemperatureWidget(degre: 15, hour: 5),
-      TemperatureWidget(degre: 1, hour: 6),
-    ],
-  );
-  
-}
 
+  Widget buildHourlyForecast() {
+    int hour = DateTime.now().hour;
+    List<Widget> list = [];
+    for (var i = 0; i < 24; i++) {
+      list.add(const SizedBox(
+        width: 13,
+      ));
+      if (i == hour) {
+        list.add(TemperatureWidget(degre: Random().nextInt(30), hour: 'Now'));
+      } else if (i < 13) {
+        list.add(TemperatureWidget(degre: Random().nextInt(30), hour: '$i AM'));
+      } else {
+        list.add(TemperatureWidget(
+            degre: Random().nextInt(30), hour: '${i - 12} PM'));
+      }
+    }
+    list.add(const SizedBox(
+      width: 13,
+    ));
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: list,
+      ),
+    );
+  }
+
+  Widget buildWeeklyForecast() {
+    List<Widget> list = [];
+    for (var i = 10; i < 18; i++) {
+      list.add(const SizedBox(
+        width: 13,
+      ));
+      list.add(TemperatureWidget(degre: Random().nextInt(30), hour: '$i/11'));
+    }
+    list.add(const SizedBox(
+      width: 13,
+    ));
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: list,
+      ),
+    );
+  }
+
+  Widget buildForecast() {
+    if (currentForecast == ForecastType.hourly) {
+      return buildHourlyForecast();
+    } else {
+      return buildWeeklyForecast();
+    }
+  }
+}
