35bba2d8f8f82eb2b52b10d0405033f8056b130b
chetanis <anis.chetouane@etu.usthb.dz>
2023-11-19 23:07:11 +0100

    adding controller

---
+++ lib/constants.dart
@@ -0,0 +1,1 @@
+const String apiUrl="https://api.openweathermap.org/data/2.5/weather?q=boumerdes&appid=42ccb03a39d70672fcdc3f178b7cfd8c&units=metric";
---
+++ lib/controller/weather_controller.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:clima_check/model/currentWeather.dart';
+import 'package:dio/dio.dart';
+
+import '../constants.dart' as constants;
+
+class WeatherController {
+  final Dio dio = Dio();
+  late CurrentWeather currentWeather;
+
+  WeatherController() {
+    initializeWeather();
+  }
+
+  Future<void> initializeWeather() async {
+    try {
+      final response = await dio.get(constants.apiUrl);
+      if (response.statusCode == 200) {
+        Map<String, dynamic> weather = json.decode(response.toString());
+        currentWeather = CurrentWeather.fromJson(weather);
+      } else {
+        throw Exception('Failed to get weather data: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to get weather data: $e');
+    }
+  }
+
+  double getTemp() {
+    return currentWeather.mainWeather!.temp!;
+  }
+}
